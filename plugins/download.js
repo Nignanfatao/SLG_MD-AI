0eaafc3b4682ef6e07fec5b2b188789054caa64c
GEEKMDXINC <herukobot67@gmail.com>
2024-10-26 02:12:46 +0100

    Update download.js

--- plugins/download.js
+++ plugins/download.js
@@ -121,7 +121,7 @@ command(
     return await message.sendFile(buff);
   },
 );
-/*
+
 
 command(
   {
@@ -145,9 +145,9 @@ command(
       "video"
     );
   }
-); */
+); 
  
-/*
+
 command(
   {
     pattern: "song",
@@ -171,7 +171,81 @@ command(
       "audio"
     );
   }
-); */ 
+); 
+
+command(
+  {
+    pattern: "yta",
+    fromMe: mode,
+    desc: "Download audio from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    if (!match) return await message.reply("Give me a youtube link");
+    if (!isUrl(match)) return await message.reply("Give me a youtube link");
+    let { dlink, title } = (
+      await getJson(
+        `https://api.thexapi.xyz/api/v1/download/youtube/audio?url=${match}`
+      )
+    ).data;
+    await message.reply(`_Downloading ${title}_`);
+    let buff = await getBuffer(dlink);
+    return await message.sendMessage(
+      message.jid,
+      buff,
+      {
+        mimetype: "audio/mpeg",
+        filename: title + ".mp3",
+      },
+      "audio"
+    );
+  }
+);
+
+command(
+  {
+    pattern: "ytv",
+    fromMe: mode,
+    desc: "Download audio from youtube",
+  },
+  async (message, match) => {
+    match = match || message.reply_message.text;
+    let url = getUrl(match)[0];
+    if (!url)
+      return await message.reply(
+        "Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p"
+      );
+    let quality = match.split(";")[1];
+    if (quality && !validateQuality(quality)) {
+      return await message.reply(
+        "Invalid Resolution \nSupported: 144p, 240p, 360p, 480p, 720p, 1080p, 1440p, 2160p"
+      );
+    } else if (!quality) quality = "360p";
+    if (!match)
+      return await message.reply(
+        "Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p"
+      );
+    if (!isUrl(match))
+      return await message.reply(
+        "Give me a youtube link\n\nExample: ytv youtube.com/watch?v=xxxxx 480p"
+      );
+    let requrl = `https://api.thexapi.xyz/api/v1/download/youtube/video?url=${url}&quality=${quality}`;
+    let response = (await getJson(requrl)).data;
+    const { dlink, title } = response;
+    console.log(response);
+    await message.reply(`_Downloading ${title}_`);
+    return await message.sendMessage(
+      message.jid,
+      dlink,
+      {
+        mimetype: "video/mp4",
+        filename: title + ".mp4",
+      },
+      "video"
+    );
+  }
+);
+
 
 command(
   {
