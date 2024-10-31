c2591a513b9664b552694167e206464366f243e0
GEEKMDXINC <herukobot67@gmail.com>
2024-10-31 18:21:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,73 @@ const fs = require("fs").promises;
 const path = require("path");
 const express = require("express");
 const config = require("./config");
+const { connect, writeSession, patch, parseDir } = require("./lib");
+const { getandRequirePlugins } = require("./lib/database/plugins");
+global.__basedir = __dirname;
+
+async function initialize() {
+  try {
+    await writeSession();
+    await parseDir(path.join(__dirname, "/lib/database/"));
+    console.log("Syncing Database");
+    await config.DATABASE.sync();
+    await parseDir(path.join(__dirname, "/plugins/"));
+    await getandRequirePlugins();
+    console.log("External Modules Installed");
+    return await connect();
+  } catch (error) {
+    console.error("Initialization error:", error);
+  }
+}
+
+async function startServer() {
+  const app = express();
+  const port = process.env.PORT || 3000;
+
+  app.get("/", (req, res) => {
+    res.send("Bot Running");
+  });
+
+  app.listen(port, () => {});
+}
+
+async function tempDir() {
+  const dir = path.join(__dirname, "temp");
+  try {
+    await fs.access(dir);
+  } catch (err) {
+    if (err.code === "ENOENT") {
+      await fs.mkdir(dir, { recursive: true });
+    }
+  }
+}
+
+async function main() {
+  try {
+    await initialize();
+    await startServer();
+    await tempDir();
+  } catch (error) {
+    console.warn("BOT SYSTEM FAILED");
+  }
+}
+
+main();
+
+
+
+
+
+
+
+
+
+
+
+/* const fs = require("fs").promises;
+const path = require("path");
+const express = require("express");
+const config = require("./config");
 const { connect, saveCreds, ensureSessionDirectory, patch, parseDir } = require("./lib");
 const { getandRequirePlugins } = require("./lib/database/plugins");
 global.__basedir = __dirname;
@@ -56,4 +123,4 @@ async function main() {
   }
 }
 
-main();
+main(); */
